bcb8630c73972dc308ccea8b1ea465e7bbc52574
Sbusiso Mnisi <smnisi@c4r12s3.wethinkcode.co.za>
2020-06-19 12:23:22 +0200

    ex01

---
+++ ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+void ft_putchar(char c) 
+{
+	write(1,&c,1);
+}
+
+void ft_print_reverse_alphabet()
+{
+	char ch = 'z';
+	while (ch >= 'a')
+	{
+		ft_putchar(ch);
+		ch--;
+	}
+}
+
+int main()
+{
+	ft_print_reverse_alphabet();
+	return 0;
+}
