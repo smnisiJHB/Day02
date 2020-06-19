da818ad5314cade4b1886e60fa4860773ca1d67b
Sbusiso Mnisi <smnisi@c4r12s3.wethinkcode.co.za>
2020-06-19 13:51:39 +0200

    ex02

---
+++ ex02/ft_print_numbers.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1,&c,1);
+}
+
+void ft_print_numbers()
+{
+	char ch = '0';
+	while(ch <= '9')
+	{
+		ft_putchar(ch);
+		ch++;
+	}
+}
+
+int main()
+{
+	ft_print_numbers();
+	return 0;
+}
+
